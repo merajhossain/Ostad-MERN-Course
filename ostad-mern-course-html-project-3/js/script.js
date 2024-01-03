f994bd344492a6b73d5b9b3fcf25dff463eef726
HOSSAIN\meraj <merajhossain29@gmail.com>
2024-01-03 12:15:59 +0600

    --

--- ostad-mern-course-html-project-3/js/script.js
+++ ostad-mern-course-html-project-3/js/script.js
@@ -1,36 +1,48 @@
 //dialer generate
 for (let index = 0; index < 60; index++) {
-    $(".clock").append('<div class="lines" style="transform:rotate('+ 6 * index +'deg)"></div>');
+  $(".clock").append(
+    '<div class="lines" style="transform:rotate(' + 6 * index + 'deg)"></div>'
+  );
 }
 
 //on move dialer
 clockCalculation = () => {
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
-    let hourHand = hour * 30 + minute * (360/720);
-    let minuteHand = minute * 6 + second * (360/3600);
-    let secondHand = second * 6;
-    // console.log('minuteHand', minuteHand);
-    $('.hour').css('transform', 'rotate('+ hourHand +'deg)');
-    $('.minute').css('transform', 'rotate('+ minuteHand +'deg)');
-    $('.second').css('transform', 'rotate('+ secondHand +'deg)');
-    $('.hourNow').html(hour % 12 || 12);
-    $('.minNow').html(minute);
-    $('.secNow').html(second > 9 ? second : '0'+second);
-}
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  let hourHand = hour * 30 + minute * (360 / 720);
+  let minuteHand = minute * 6 + second * (360 / 3600);
+  let secondHand = second * 6;
+  // console.log('minuteHand', minuteHand);
+  $(".hour").css("transform", "rotate(" + hourHand + "deg)");
+  $(".minute").css("transform", "rotate(" + minuteHand + "deg)");
+  $(".second").css("transform", "rotate(" + secondHand + "deg)");
+  $(".hourNow").html(hour % 12 || 12);
+  $(".minNow").html(minute);
+  $(".secNow").html(second > 9 ? second : "0" + second);
+};
 
 dateGenerate = () => {
-    let date = new Date();
-    $('.date').html(date.getDay() > 9 ? date.getDay() : '0'+date.getDay());
-    const formattedMonth = (date.getMonth() < 10 ? '0' : '') + (date.getMonth() + 1);
-    $('.month').html(formattedMonth);
-    $('.year').html(date.getFullYear());
-}
+  let date = new Date();
+  $(".date").html(date.getDay() > 9 ? date.getDay() : "0" + date.getDay());
+  const formattedMonth =
+    (date.getMonth() < 10 ? "0" : "") + (date.getMonth() + 1);
+  $(".month").html(formattedMonth);
+  $(".year").html(date.getFullYear());
+};
 
 //call function every one second
 setInterval(() => {
-    dateGenerate();
-    clockCalculation();
+  dateGenerate();
+  clockCalculation();
 }, 100);
+
+
+$(document).ready(function () {
+    $(document).ready(function() {
+        var audio = $("#myAudio")[0];
+        audio.autoplay = true;
+        audio.load();
+    });
+});
